57994b93441e0f8e0ed10769b9099573f1a194f4
maxhithub12 <yshanks7@gmail.com>
2024-06-13 19:03:01 -0600

    Edicion de modulos membresia

--- gimasio2/FrontEnd/Gimnasio-FrontEnd/src/components/Membresia.vue
+++ gimasio2/FrontEnd/Gimnasio-FrontEnd/src/components/Membresia.vue
@@ -2,4 +2,53 @@
     <h1 class="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-black">
         Membresías</h1>
     <hr>
+    <br>
+    <form>
+        <input
+            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
+            type="text" placeholder="Nombre Membresia"
+        />
+        
+        <select id="countries" class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5">
+            <option selected>Tipo</option>
+            <option value="M">Basica</option>
+            <option value="F">Media</option>
+            <option value="NB">Premium</option>
+        </select>
+        
+        <input
+            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+            type="number" placeholder="Precio"
+        />
+    
+        
+        <a href="/" class="col-span-2" >
+            <button
+                class="mt-5 tracking-wide font-semibold bg-blue-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
+                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy="7" r="4" />
+                    <path d="M20 8v6M23 11h-6" />
+                </svg>
+                <span class="ml-3 uppercase">
+                    Registrar
+                </span>
+            </button>
+        </a>
+    </form>
+
+    <div>
+        <Tmembresia/>
+    </div>
+    
 </template>
+
+<script>
+    import Tmembresia from './tmembresias.vue'
+        export default{
+            components:{
+                Tmembresia
+            }
+        }
+</script>
---
+++ gimasio2/FrontEnd/Gimnasio-FrontEnd/src/components/tmembresias.vue
@@ -0,0 +1,101 @@
+<template>
+   
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+     <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+         <div>
+             <!-- Dropdown menu -->
+             <div id="dropdownAction" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
+                     <li>
+                         <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reward</a>
+                     </li>
+                     <li>
+                         <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Promote</a>
+                     </li>
+                     <li>
+                         <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Activate account</a>
+                     </li>
+                 </ul>
+                 <div class="py-1">
+                     <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete User</a>
+                 </div>
+             </div>
+         </div>
+         <label for="table-search" class="sr-only">Search</label>
+     </div>
+     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+             <tr>
+                 <th scope="col" class="p-4">
+                     <div class="flex items-center">
+                         <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                         <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                     </div>
+                 </th>
+                 
+                 <th scope="col" class="px-6 py-3">Nombre</th>
+                 <th scope="col" class="px-6 py-3">Tipo</th>
+                 <th scope="col" class="px-6 py-3">Precio</th>
+
+                 <th scope="col" class="px-6 py-3">Eliminar</th>
+                 <th scope="col" class="px-6 py-3">Editar</th>
+             </tr>
+         </thead>
+         <tbody>
+             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                 <td class="w-4 p-4">
+                     <div class="flex items-center">
+                         <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                         <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                     </div>
+                 </td>
+                 
+                 <td class="px-6 py-4">Membresia1</td>
+                 <td class="px-6 py-4">Basica</td>
+                 <td class="px-6 py-4">100</td>
+                 
+                
+                 
+                 <td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-red-500 hover:underline">Eliminar</a></td>
+                 <td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a></td>
+             </tr>
+             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                 <td class="w-4 p-4">
+                     <div class="flex items-center">
+                         <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                         <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
+                     </div>
+                 </td>
+                 
+                 <td class="px-6 py-4">Membresia2</td>
+                 <td class="px-6 py-4">Premium</td>
+                 <td class="px-6 py-4">500</td>
+                 
+                 
+                 <td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-red-500 hover:underline">Eliminar</a></td>
+                 <td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a></td>
+             </tr>
+             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                 <td class="w-4 p-4">
+                     <div class="flex items-center">
+                         <input id="checkbox-table-search-3" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                         <label for="checkbox-table-search-3" class="sr-only">checkbox</label>
+                     </div>
+                 </td>
+                 
+                 <td class="px-6 py-4">Membresia3</td>
+                 <td class="px-6 py-4">Media</td>
+                 <td class="px-6 py-4">200</td>
+                 
+                 
+                 <td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-red-500 hover:underline">Eliminar</a></td>
+                 <td class="px-6 py-4"><a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a></td>
+             </tr>
+         </tbody>
+     </table>
+ </div>
+ 
+ 
+ 
+ </template>
